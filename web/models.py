f976dfe29eedcbef0cd5c16f726735e57d913989
fetgrigory <97grfetkulin@mail.ru>
2025-07-27 22:55:53 +0300

    add ch

--- web/models.py
+++ web/models.py
@@ -18,6 +18,9 @@ class User(Base):
         Base ([type]): [description]
     """
     __tablename__ = "users"
+    # id (int): Unique identifier for each user
     id = Column(Integer, primary_key=True, index=True)
+    # Username, required for login
     username = Column(String(80), unique=True, nullable=False)
+    # User's password
     password = Column(String(80), nullable=False)
