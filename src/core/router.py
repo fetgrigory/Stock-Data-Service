bd06ba8cde7c09ce8f9a1a65ae84bf9827c8cbb6
fetgrigory <97grfetkulin@mail.ru>
2025-10-01 14:33:17 +0300

    feat(core): add core folder with main page router; add templates folder in src

---
+++ src/core/router.py
@@ -0,0 +1,24 @@
+'''
+This module make
+
+Author: Fetkulin Grigory, Fetkulin.G.R@yandex.ru
+Starting 01/10/2025
+Ending //
+
+'''
+# Installing the necessary libraries
+import os
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+
+# Templates and statics
+templates = Jinja2Templates(directory=os.path.join("src", "templates"))
+
+
+# Home page route
+@router.get("/", response_class=HTMLResponse)
+async def homepage(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ import uvicorn
 from fastapi import FastAPI
 from recipients.router import router as recipients_router
 from smtp.router import router as smtp_router
+from core.router import router as page_router
 from database import create_recipients_table
 
 app = FastAPI()
@@ -19,6 +20,7 @@ create_recipients_table()
 # Connecting routers
 app.include_router(recipients_router)
 app.include_router(smtp_router)
+app.include_router(page_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True)
