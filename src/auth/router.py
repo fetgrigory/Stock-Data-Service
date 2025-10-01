1e005e023ab2a2d8f476d6d43e6f1e17461131a9
fetgrigory <97grfetkulin@mail.ru>
2025-10-01 18:17:11 +0300

    Add login page route

---
+++ src/auth/router.py
@@ -0,0 +1,24 @@
+'''
+This module make
+
+Author: Fetkulin Grigory, Fetkulin.G.R@yandex.ru
+Starting 01/10/2025
+Ending //
+
+'''
+# Installing the necessary libraries
+import os
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+
+# Templates
+templates = Jinja2Templates(directory=os.path.join("src", "templates"))
+
+
+# login page route
+@router.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ from fastapi.staticfiles import StaticFiles
 from recipients.router import router as recipients_router
 from smtp.router import router as smtp_router
 from core.router import router as page_router
+from auth.router import router as auth_router
 from database import create_recipients_table
 
 app = FastAPI()
@@ -24,6 +25,7 @@ create_recipients_table()
 app.include_router(recipients_router)
 app.include_router(smtp_router)
 app.include_router(page_router)
+app.include_router(auth_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True)
