87ad61776249a56b65ee3356195a22b57d206e12
fetgrigory <97grfetkulin@mail.ru>
2025-10-03 21:02:52 +0300

    chore: add __init__.py, enable dotenv, update requirements
    - added __init__.py files to package folders
    - enabled dotenv for database config
    - updated requirements.txt (authx library)

--- src/database.py
+++ src/database.py
@@ -9,6 +9,9 @@ Ending //
 # Installing the necessary libraries
 import os
 import psycopg2
+from dotenv import load_dotenv
+# Loading environment variables from .env
+load_dotenv()
 
 
 def db_connect():
