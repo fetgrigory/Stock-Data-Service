bfb0cc4bb17da0b3686f4626367b5d93a8d7490a
fetgrigory <97grfetkulin@mail.ru>
2025-10-04 19:41:09 +0300

    Fix email sending: personalize greetings for each recipient

--- src/email/email_sender.py
+++ src/email/email_sender.py
@@ -33,57 +33,56 @@ class EmailSender:
         self.recipients = get_all_recipients()
         if not self.recipients:
             logging.warning("Список получателей пуст!")
-        # Dynamic subject and text of the letter
-        for recipient in self.recipients:
-            name = get_recipient_name(recipient)
-        self.subject = f'Архив с биржевыми данными от {datetime.now():%d.%m.%Y}'
-        self.text = f'Здравствуйте, {name}! Во вложении находится архив с актуальными данными.'
-        self.html = f'<html><head></head><body><p>{self.text}</p></body></html>'
 
-    # Send email with file attachment
+        # Send email with file attachment
     def send_email_with_attachment(self, filepath):
         """AI is creating summary for send_email_with_attachment
 
         Args:
             filepath ([type]): [description]
-
-        Returns:
-            [type]: [description]
         """
-        # Prepare file metadata
-        try:
-            basename = os.path.basename(filepath)
-            filesize = os.path.getsize(filepath)
-            # Create multipart message container
-            msg = MIMEMultipart('alternative')
-            msg['Subject'] = self.subject
-            msg['From'] = f'Python script <{self.sender}>'
-            msg['To'] = ', '.join(self.recipients)
-            msg['Reply-To'] = self.sender
-            msg['Return-Path'] = self.sender
-            msg['X-Mailer'] = f'Python/{python_version()}'
-            # Create text and HTML email parts
-            part_text = MIMEText(self.text, 'plain')
-            part_html = MIMEText(self.html, 'html')
-            # Create file attachment part
-            part_file = MIMEBase('application', f'octet-stream; name="{basename}"')
-            with open(filepath, "rb") as f:
-                part_file.set_payload(f.read())
-            # Add file headers
-            part_file.add_header('Content-Description', basename)
-            part_file.add_header('Content-Disposition', f'attachment; filename="{basename}"; size={filesize}')
-            encoders.encode_base64(part_file)
-            # Attach all parts to message
-            msg.attach(part_text)
-            msg.attach(part_html)
-            msg.attach(part_file)
-            # Send email via SMTP server
-            with smtplib.SMTP_SSL(self.server, self.port) as mail:
-                mail.login(self.user, self.password)
-                mail.sendmail(self.sender, self.recipients, msg.as_string())
+        basename = os.path.basename(filepath)
+        filesize = os.path.getsize(filepath)
+        # Dynamic subject and text of the letter
+        for recipient_email in self.recipients:
+            try:
+                # Get the name of a specific recipient
+                name = get_recipient_name(recipient_email)
+                subject = f'Архив с биржевыми данными от {datetime.now():%d.%m.%Y}'
+                text = f'Здравствуйте, {name}! Во вложении находится архив с актуальными данными.'
+                html = f'<html><head></head><body><p>{text}</p></body></html>'
 
-            logging.info("Email sent successfully with attachment: %s", filepath)
-            return True
-        except Exception as e:
-            logging.error("Failed to send email: %s", e)
-            return False
+                # Create multipart message container
+                msg = MIMEMultipart('alternative')
+                msg['Subject'] = subject
+                msg['From'] = f'Python script <{self.sender}>'
+                msg['To'] = recipient_email
+                msg['Reply-To'] = self.sender
+                msg['Return-Path'] = self.sender
+                msg['X-Mailer'] = f'Python/{python_version()}'
+
+                # Create text and HTML email parts
+                part_text = MIMEText(text, 'plain')
+                part_html = MIMEText(html, 'html')
+
+                # Add file headers
+                part_file = MIMEBase('application', f'octet-stream; name="{basename}"')
+                with open(filepath, "rb") as f:
+                    part_file.set_payload(f.read())
+                part_file.add_header('Content-Description', basename)
+                part_file.add_header('Content-Disposition', f'attachment; filename="{basename}"; size={filesize}')
+                encoders.encode_base64(part_file)
+
+                # Attach all parts to message
+                msg.attach(part_text)
+                msg.attach(part_html)
+                msg.attach(part_file)
+
+                # Send email via SMTP server
+                with smtplib.SMTP_SSL(self.server, self.port) as mail:
+                    mail.login(self.user, self.password)
+                    mail.sendmail(self.sender, recipient_email, msg.as_string())
+                logging.info("Email sent successfully to: %s", recipient_email)
+
+            except Exception as e:
+                logging.error("Failed to send email to %s: %s", recipient_email, e)
