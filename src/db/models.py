f50a979d00d77b1d48edacfc30645055b05161a5
fetgrigory <97grfetkulin@mail.ru>
2025-11-06 22:23:29 +0300

    Add recipients and SMTP settings tables; fix users table text columns

--- src/db/models.py
+++ src/db/models.py
@@ -7,7 +7,7 @@ Ending //
 
 '''
 # Installing the necessary libraries
-from sqlalchemy import Table, Column, Integer, String, MetaData
+from sqlalchemy import Table, Column, Integer, Text, MetaData
 # Creating a metadata object to hold table definitions
 metadata_obj = MetaData()
 # Defining the 'users' table structure
@@ -15,6 +15,26 @@ users_table = Table(
     "users",
     metadata_obj,
     Column("id", Integer, primary_key=True),
-    Column("username", String),
-    Column("password", String),
+    Column("username", Text),
+    Column("password", Text),
+)
+# Defining the 'recipients' table structure
+recipients_table = Table(
+    "recipients",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("name", Text),
+    Column("email", Text),
+)
+
+# Defining the 'smtp_settings' table structure
+smtp_settings_table = Table(
+    "smtp_settings",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("server", Text),
+    Column("port", Integer),
+    Column("username", Text),
+    Column("password", Text),
+    Column("sender", Text),
 )
