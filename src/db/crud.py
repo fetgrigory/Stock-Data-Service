80e536dcc90a68426dce339ed156cfe997886953
Fetkulin Grigory <97grfetkulin@mail.ru>
2025-11-08 22:20:36 +0300

    refactor: unify ORM insert functions to return inserted ID

--- src/db/crud.py
+++ src/db/crud.py
@@ -9,7 +9,7 @@ Ending //
 # Installing the necessary libraries
 from sqlalchemy.orm import Session
 from src.db.database_orm import engine
-from src.db.models import User
+from src.db.models import User, Recipient
 
 
 # Adding a new user
@@ -24,6 +24,8 @@ def insert_user(username: str, password: str):
         user = User(username=username, password=password)
         session.add(user)
         session.commit()
+        user_id = user.id
+        return user_id
 
 
 # Getting the user's by username
@@ -38,3 +40,22 @@ def get_user_by_username(username: str):
     """
     with Session(engine) as session:
         return session.query(User).filter(User.username == username).first()
+
+
+# Adding a new recipient
+def insert_recipient(name: str, email: str):
+    """AI is creating summary for insert_recipient
+
+    Args:
+        name (str): [description]
+        email (str): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    with Session(engine) as session:
+        recipient = Recipient(name=name, email=email)
+        session.add(recipient)
+        session.commit()
+        recipient_id = recipient.id
+    return recipient_id
