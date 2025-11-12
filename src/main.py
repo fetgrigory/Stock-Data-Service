a59397c5fee723fd84240c1c8eba98288dc0c1b7
Fetkulin Grigory <97grfetkulin@mail.ru>
2025-11-12 23:24:26 +0300

    remove: delete create_recipients_table() call from main

--- src/main.py
+++ src/main.py
@@ -14,13 +14,10 @@ from src.auth.router import router as auth_router
 from src.core.router import router as page_router
 from src.recipients.router import router as recipients_router
 from src.email.router import router as smtp_router
-from src.db.database import create_recipients_table
 
 app = FastAPI()
 # Connecting static files
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
-# Creating a table at the start of the application
-create_recipients_table()
 # Connecting routers
 app.include_router(recipients_router)
 app.include_router(smtp_router)
