c3d44d198dec6e61ea60e2763ad9a8d03cfb72d2
fetgrigory <97grfetkulin@mail.ru>
2026-04-03 23:41:31 +0300

    fix(database): rename HOST to DB_HOST in connection settings

--- src/db/database.py
+++ src/db/database.py
@@ -1,12 +1,3 @@
-'''
-This module make
-
-Author: Fetkulin Grigory, Fetkulin.G.R@yandex.ru
-Starting 25/09/2025
-Ending //
-
-'''
-# Installing the necessary libraries
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
@@ -15,13 +6,13 @@ from src.db.models import Base
 # Loading variables from .env
 load_dotenv()
 # Getting variables from the environment
-HOST = os.getenv("HOST")
+DB_HOST = os.getenv("DB_HOST")
 DBNAME = os.getenv("DBNAME")
 USER = os.getenv("USER")
 PASSWORD = os.getenv("PASSWORD")
 PORT = os.getenv("PORT")
 # Creating a connection string
-DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}"
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{DB_HOST}:{PORT}/{DBNAME}"
 # Creating an engine
 sync_engine = create_engine(
     DATABASE_URL,
